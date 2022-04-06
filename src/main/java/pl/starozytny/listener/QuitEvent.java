9e065e8b2ffe4ee34a98e3e5af0cac4e2d90ba59
BuildTools <unconfigured@null.spigotmc.org>
2022-04-06 22:33:05 +0200

    initial commit

--- src/main/java/pl/starozytny/listener/QuitEvent.java
+++ src/main/java/pl/starozytny/listener/QuitEvent.java
@@ -7,15 +7,11 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.mineacademy.fo.Common;
-import org.mineacademy.fo.PlayerUtil;
 import org.mineacademy.fo.remain.Remain;
 import pl.starozytny.Sprawdzanie;
 import pl.starozytny.file.ConfigFile;
-import pl.starozytny.file.Messages;
 
 import java.util.List;
-import java.util.Objects;
-import java.util.stream.Collectors;
 
 public class QuitEvent implements Listener {
 
@@ -23,19 +19,14 @@ public class QuitEvent implements Listener {
 	List<String> INFORM_LOGOUT;
 
 	@EventHandler(priority = EventPriority.MONITOR)
-	public void CommandBlock(PlayerQuitEvent event) {
+	public void playerQuit(PlayerQuitEvent event) {
 
 
 		if (addedPlayers.contains(event.getPlayer().getName())) {
-			if (Bukkit.dispatchCommand(Bukkit.getConsoleSender(), ConfigFile.getInstance().COMMAND_LOGOUT.replace("{player}", event.getPlayer().getName()))) {
-				Common.tell(event.getPlayer(), Messages.Error.COMMAND_BLOCKED);
-				for (final Player online : Remain.getOnlinePlayers()) {
-					if (PlayerUtil.hasPerm(online, "sprawdz.admin")) {
-						INFORM_LOGOUT = Messages.Information.INFORM_PLAYER_LOGOUT.stream().filter(Objects::nonNull).map(rawList -> rawList.
-								replace("{player}", event.getPlayer().getName())).collect(Collectors.toList());
-						Common.tell(online, INFORM_LOGOUT);
-					}
-				}
+			Bukkit.dispatchCommand(Bukkit.getConsoleSender(), ConfigFile.getInstance().COMMAND_LOGOUT.replace("{player}", event.getPlayer().getName()));
+			addedPlayers.remove(event.getPlayer().getName());
+			for (final Player online : Remain.getOnlinePlayers()) {
+				Common.tell(online, INFORM_LOGOUT);
 			}
 		}
 	}
